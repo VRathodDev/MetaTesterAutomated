abe567106b492f51b2520be202dbefc43149540a
Vipul Rathod <vrathod@magsw.com>
2021-09-09 14:44:21 +0530

    [MetaTesterAutomation][1.0.3] - Release Version

--- RemoteConnection.py
+++ RemoteConnection.py
@@ -4,21 +4,21 @@ from GenUtility import isNoneOrEmpty
 
 class RemoteConnection:
     def __init__(self, in_HostAddress: str, in_UserName: str, in_Password):
-        self.mHostAddress = in_HostAddress
-        self.mUserName = in_UserName
-        self.mPassword = in_Password
+        self.__mHostAddress = in_HostAddress
+        self.__mUserName = in_UserName
+        self.__mPassword = in_Password
 
     def connect(self):
         """
         Connects to the given host via provided credentials \n
         :return: Returns True if successfully connected else False
         """
-        if not isNoneOrEmpty(self.mHostAddress, self.mUserName, self.mPassword):
-            unc = ''.join(['\\\\', self.mHostAddress])
+        if not isNoneOrEmpty(self.__mHostAddress, self.__mUserName, self.__mPassword):
+            unc = ''.join(['\\\\', self.__mHostAddress])
             netResource = win32wnet.NETRESOURCE()
             netResource.lpRemoteName = unc
             try:
-                win32wnet.WNetAddConnection2(netResource, self.mPassword, self.mUserName, 0)
+                win32wnet.WNetAddConnection2(netResource, self.__mPassword, self.__mUserName, 0)
             except Exception as error:
                 if isinstance(error, win32wnet.error):
                     if 1219 in error.args:
@@ -43,7 +43,7 @@ class RemoteConnection:
         Disconnects from the given host \n
         :return: Returns True if successfully disconnected else False
         """
-        unc = ''.join(['\\\\', self.mHostAddress])
+        unc = ''.join(['\\\\', self.__mHostAddress])
         try:
             win32wnet.WNetCancelConnection2(unc, 0, 0)
         except Exception as error:
